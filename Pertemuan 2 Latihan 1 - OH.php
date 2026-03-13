3231545a116450b9e0cdb94a8f1e201906e0b3d5
oksariohegar-dot <oksariohegar@gmail.com>
2026-03-13 21:16:56 +0700

    Add files via upload

---
+++ Pertemuan 2 Latihan 1 - OH.php
@@ -0,0 +1,16 @@
+<?php
+    $a = 1;
+    $b = 2;
+    $c = $a . $b;
+    //nilai $c adalah 12, menggabungkan angka 1 dan 2
+    
+    $d = $c + 1;
+    echo $d;
+    //nilai $d adalah 13, menggabungkan angka 12 dan 1
+
+    $e = "Number";
+    $f = $e . $d;
+    echo $f;
+    //nilai $f adalah "Number 13", menggabungkan string "Number " dan angka 13
+
+?>
